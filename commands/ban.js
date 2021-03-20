4489b87b045f458eb3af0900529bee4b1cf033c1
Peter Hilton <firegeekjr@gmail.com>
2021-03-19 23:39:28 -0700

    Update ban.js
    Revamped Ban Command

--- commands/ban.js
+++ commands/ban.js
@@ -1,8 +1,7 @@
-const { Message } = require('discord.js');
+const { Message, MessageEmbed } = require('discord.js');
 const fs = require('fs');
 const words = require('./words')
 const word = words.words
-
 
 module.exports = {
     name: 'ban',
@@ -20,30 +19,64 @@ module.exports = {
           if (message.guild.members.cache.get(args[0])) {
             const bantarget = message.guild.members.cache.get(args[0])
              bantarget.ban()
-             message.channel.send(`<@!${args[0]}> Has been banned`)
-             client.channels.cache.get(`812025615732572230`).send(`${message.author} has banned ${target} | ${args[0]} |
-             ${message.channel.guild.name}`)
+              const embed = new MessageEmbed()
+              .setTitle(`Ban Info`)
+              .setColor(`#FF0000`)
+              .addFields(
+               { name: `Moderator`, value: `${message.author}`, inline: true},
+               { name: `Banned`, value: `${args[0]}`, inline: true},
+           )
+             message.channel.send(embed)
+             client.channels.cache.get(`812025615732572230`).send(embed)
              return;
           }
           else if (target) {
             const targetMember = message.guild.members.cache.get(target.id)
             if (targetMember.bannable) {
-            targetMember.ban() 
-            message.channel.send(`${target} has been banned`)
-            client.channels.cache.get('812025615732572230').send(`${message.author} has banned ${target} | ${targetMember} |
-            ${message.channel.guild.name}`)
+             targetMember.ban() 
+              const embed = new MessageEmbed()
+              .setTitle(`Ban Info`)
+              .setColor(`#FF0000`)
+              .setThumbnail(message.guild.iconURL())
+              .addFields(
+              { name: `Moderator`, value: `${message.author}`, inline: true},
+              { name: `Banned`, value: `${targetMember}`, inline: true},
+         )
+           message.channel.send(embed)
+           client.channels.cache.get(`812025615732572230`).send(embed)
+           return;
             }
             else {
-              message.channel.send(`User Cannot Be Banned`)
+              const embed = new MessageEmbed()
+              .setTitle(`Ban Info`)
+              .setColor(`#FF0000`)
+              .setThumbnail(message.guild.iconURL())
+              .addFields(
+               { name: `${message.author.tag}`, value: `Could not ban ${target}`, inline: true}
+           )
+             message.channel.send(embed)
+             return;
             }
            
           } else {
-            message.channel.send(`${message.author} Please specify someone to ban.`)
+              const embed = new MessageEmbed()
+              .setTitle(`Ban Info`)
+              .setColor(`#FF0000`)
+              .setThumbnail(message.guild.iconURL())
+              .addFields(
+               { name: `${message.author.tag}`, value: `Please specify a user to ban`, inline: true}
+           )
+             message.channel.send(embed)
           }
         } else {
-          message.channel.send(
-            `${tag} You do not have permission to use this command.`
-          )
+          const embed = new MessageEmbed()
+          .setTitle(`Ban Info`)
+          .setColor(`#FF0000`)
+          .setThumbnail(message.guild.iconURL())
+          .addFields(
+           { name: `${message.author.tag}`, value: `You cant use that command!`, inline: true}
+       )
+         message.channel.send(embed)
         }
       }
     } 
